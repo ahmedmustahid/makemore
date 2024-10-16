93d4a0c6ce9a80990b587a74a8c3b93a2c91b283
ahmedmustahid <einsteinium25@gmail.com>
2024-10-16 09:19:19 +0900

    flatten consecutive words

--- makemore/flatten.py
+++ makemore/flatten.py
@@ -1,8 +1,27 @@
-class Flatten:
+import torch
+
+class FlattenConsecutive:
+
+    def __init__(self, n):
+        self.n = n
+
     def __call__(self, x):
-        self.out = x.view(x.shape[0], -1)
+        B, T, C = x.shape
+        x = x.view(B, T//self.n, C*self.n)
+
+        if x.shape[1]==1:
+            x = torch.squeeze(x, dim=1)
+        self.out = x
         return self.out
     
     def parameters(self):
         return []
-        
+
+
+
+if __name__=="__main__":
+    e = torch.randn(4, 8, 20)
+    f = FlattenConsecutive(2)
+    e = f(e)
+
+    print(e.shape)
