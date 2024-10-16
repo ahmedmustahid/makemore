c6b206ce9737f4476ea111fa3e80c66f28fba17a
ahmedmustahid <einsteinium25@gmail.com>
2024-10-16 10:01:08 +0900

    batchnorm for multiple batch dimension

--- makemore/batchNorm1d.py
+++ makemore/batchNorm1d.py
@@ -3,22 +3,29 @@ import torch
 
 
 class BatchNorm1d:
-    def __init__(self, num_features, eps=1e-05, momentum=0.1, training=True):
+    def __init__(self, dim, eps=1e-05, momentum=0.1, training=True):
         self.eps = eps
         self.momentum = momentum
 
-        self.gamma = torch.ones(num_features)
-        self.beta = torch.zeros(num_features)
+        self.gamma = torch.ones(dim)
+        self.beta = torch.zeros(dim)
 
         self.training = training
 
-        self.running_mean = torch.zeros(num_features)
-        self.running_var = torch.ones(num_features)
+        self.running_mean = torch.zeros(dim)
+        self.running_var = torch.ones(dim)
 
     def __call__(self, x):
+
+        if x.ndim==2:
+            dim = 0
+        elif x.ndim==3:
+            dim = (0,1)
+        else: 
+            dim = 0
         if self.training:
-            xmean = x.mean(0, keepdim=True)
-            xvar = x.var(0, keepdim=True)
+            xmean = x.mean(dim, keepdim=True)
+            xvar = x.var(dim, keepdim=True)
         else:
             xmean = self.running_mean
             xvar = self.running_var
@@ -45,5 +52,12 @@ if __name__=="__main__":
     print(f"{x=}")
 
     bn = BatchNorm1d(3)
+    print("bn: ", bn(x))
+
+    x = torch.randn(3,2)
+
+    print(f"{x=}")
+
+    bn = BatchNorm1d((3,2))
     print("bn: ", bn(x))
 
